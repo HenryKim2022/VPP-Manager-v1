69f21a0748c00a56c217c857e98e15319e19d9ea
HenryKim2022 <autromatron@gmail.com>
2024-07-21 01:09:22 +0700

    "Updated APP_NAME in .env file and VPP Manager references to VPP Monitor in .env file and v_sidenav.blade.php"

--- resources/views/layouts/userpanels/v_sidenav.blade.php
+++ resources/views/layouts/userpanels/v_sidenav.blade.php
@@ -4,9 +4,9 @@
         <ul class="nav navbar-nav flex-row">
             <li class="nav-item mr-auto"><a class="navbar-brand"
                     href=""><span class="brand-logo">  <!-- route('userPanels.dashboard') }} -->
-                        <img src="{{ asset('public/assets/logo/vp_logo.svg') }}" alt="VPP Manager Logo">
+                        <img src="{{ asset('public/assets/logo/vp_logo.svg') }}" alt="VPP Monitor Logo">
                        </span>
-                    <h2 class="brand-text">VPP Manager</h2>
+                    <h2 class="brand-text">VPP Monitor</h2>
                 </a></li>
             <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i
                         class="d-block d-xl-none text-primary toggle-icon font-medium-4" data-feather="x"></i><i
