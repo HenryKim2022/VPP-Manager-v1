f30274bf2a4e34ea8150178da7a0cd9f8163f010
HenryKim2022 <autromatron@gmail.com>
2024-10-09 14:42:20 +0700

    Created MonitoringController

---
+++ app/Http/Controllers/UserPanels/Manage/MonitoringController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\UserPanels\Manage;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MonitoringController extends Controller
+{
+    //
+}
